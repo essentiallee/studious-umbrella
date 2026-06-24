08c29bfd901e5dcd19c4f199b3f6ca9d63925db9
Jae Hyun Lee <jhlee95424@gmail.com>
2026-06-23 23:36:47 -0400

    api added

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -1,1 +1,4 @@
-console.log("this works");
+console.log("Hello, World!");
+const API_KEY = config.WEATHER_API_KEY;
+const CITY_ID = "524901";
+const API_URL = `http://api.openweathermap.org/data/2.5/forecast/city?id=${CITY_ID}&APPID=${API_KEY}`;
