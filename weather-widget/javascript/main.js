4ebf8108364ae9f09a288a634dad69190d6c790b
Jae Hyun Lee <jhlee95424@gmail.com>
2026-06-24 00:02:44 -0400

    wrapped in getweatherdata

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -2,6 +2,10 @@ console.log("Hello World!");
 
 const API_KEY = config.WEATHER_API_KEY;
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?zip=10128,us&appid=${API_KEY}&units=imperial`)
-    .then(response => response.json())
-    .then(data => console.log(data));
+function getWeatherData() {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?zip=10128,us&appid=${API_KEY}&units=imperial`)
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
+
+getWeatherData();
