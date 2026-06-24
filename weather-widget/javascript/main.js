ea1d658860d19c67b51452f3cc237e98139310d7
Jae Hyun Lee <jhlee95424@gmail.com>
2026-06-23 23:55:06 -0400

    main js updated

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -1,4 +1,7 @@
-console.log("Hello, World!");
+console.log("Hello World!");
+
 const API_KEY = config.WEATHER_API_KEY;
-const CITY_ID = "524901";
-const API_URL = `http://api.openweathermap.org/data/2.5/forecast/city?id=${CITY_ID}&APPID=${API_KEY}`;
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?zip=10128,us&appid=${API_KEY}&units=imperial`)
+    .then(response => response.json())
+    .then(data => console.log(data));
