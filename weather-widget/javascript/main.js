1e5417a81b81c0de62a96bcc02c4c51942dd0e3f
Jae Hyun Lee <jhlee95424@gmail.com>
2026-06-25 18:47:27 -0400

    Events and UIs
    Inveractive Web Submission

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -1,11 +1,25 @@
 console.log("Hello World!");
 
-const API_KEY = config.WEATHER_API_KEY;
+let btn = document.querySelector(".search-button");
+let input = document.querySelector(".zipcode");
 
-function getWeatherData() {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?zip=10128,us&appid=${API_KEY}&units=imperial`)
-        .then(response => response.json())
-        .then(data => console.log(data));
+function getWeatherData(zip) {
+    let API_KEY = config.WEATHER_API_KEY;
+    let API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${API_KEY}&units=imperial`;
+    
+    fetch(API_ENDPOINT)
+        .then((response) => response.json())
+        .then(data => {
+            let local_weather_data = data;
+                console.log(local_weather_data)
+        })
 }
 
-getWeatherData();
+function getZipCode(e) {
+    e.preventDefault();
+
+    let ZIP_CODE = input.value;
+    getWeatherData(ZIP_CODE);
+};
+
+btn.addEventListener("click", getZipCode);
