174a302140557574b219976711a64aec2f7bfc14
Jae Hyun Lee <jhlee95424@gmail.com>
2026-06-25 18:55:06 -0400

    Interactive UI (edit 01)

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -1,19 +1,19 @@
 console.log("Hello World!");
 
+const API_KEY = config.WEATHER_API_KEY;
+
 let input = document.querySelector(".zipcode");
 let btn = document.querySelector(".search-button");
 
-
 function getWeatherData(zip) {
-    let API_KEY = config.WEATHER_API_KEY;
     let API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${API_KEY}&units=imperial`;
-    
+
     fetch(API_ENDPOINT)
-        .then((response) => response.json())
+        .then(response => response.json())
         .then(data => {
             let local_weather_data = data;
-                console.log(local_weather_data)
-        })
+            console.log(local_weather_data);
+        });
 }
 
 function getZipCode(e) {
@@ -21,6 +21,6 @@ function getZipCode(e) {
 
     let ZIP_CODE = input.value;
     getWeatherData(ZIP_CODE);
-};
+}
 
 btn.addEventListener("click", getZipCode);
