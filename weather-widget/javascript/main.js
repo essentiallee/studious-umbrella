27cdf20901745029d08e29ccb71ed6d3e8a17709
Jae Hyun Lee <jhlee95424@gmail.com>
2026-06-25 19:33:41 -0400

    DOM Manipulation

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -2,25 +2,41 @@ console.log("Hello World!");
 
 const API_KEY = config.WEATHER_API_KEY;
 
+// access elements in the DOM
 let input = document.querySelector(".zipcode");
 let btn = document.querySelector(".search-button");
+let form = document.querySelector("form");
 
-function getWeatherData(zip) {
-    let API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${API_KEY}&units=imperial`;
+let CITY_NAME = document.querySelector(".city_name");
+let CITY_TEMP = document.querySelector(".temperature");
+
+let image = document.querySelector("img");
+
+// write a function to get weather data
+const getWeatherData = (zip) => {
+    const API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${API_KEY}&units=imperial`;
 
     fetch(API_ENDPOINT)
         .then(response => response.json())
         .then(data => {
             let local_weather_data = data;
+            let WEATHER_ICON = local_weather_data.weather[0].icon
+            image.setAttribute('src', `https://openweathermap.org/img/wn/${WEATHER_ICON}@2x.png`)
             console.log(local_weather_data);
+
+            CITY_NAME.textContent = local_weather_data.name;
+            CITY_TEMP.textContent = Math.round(local_weather_data.main.temp) + " °F";
         });
 }
 
-function getZipCode(e) {
+const getZipcode = (e) => {
     e.preventDefault();
 
     let ZIP_CODE = input.value;
     getWeatherData(ZIP_CODE);
+
+    form.reset();
+    input.focus();
 }
 
-btn.addEventListener("click", getZipCode);
+btn.addEventListener("click", getZipcode);
